9b40b6956c846bcb66c19e08758807fbf926318d
Amanda Wagner <aawagner85@gmail.com>
2015-01-10 10:25:33 -0800

    Error partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,49 @@
 class PostsController < ApplicationController
-  def index
+  before_action :set_post, only: [:show, :edit, :update]
+  #1. set up instance variable for action
+  #2. redirect based on some condition
 
+  def index
+    @posts = Post.all
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.creator = User.first
+    @post.category_ids = 1
+    
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'The post has been updated.'
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids: [])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
